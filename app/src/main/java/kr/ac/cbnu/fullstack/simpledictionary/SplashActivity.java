8e7f08c6efb015dc7d6bddae88e25bcd721e6365
ParkJiyoung <realplay23@naver.com>
2016-04-22 19:21:57 +0900

    [Create] SplashActivity, Splash Logo

--- app/src/main/java/kr/ac/cbnu/fullstack/simpledictionary/SplashActivity.java
+++ app/src/main/java/kr/ac/cbnu/fullstack/simpledictionary/SplashActivity.java
@@ -1,13 +1,31 @@
 package kr.ac.cbnu.fullstack.simpledictionary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        android.os.Handler handler = new android.os.Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
     }
 }
